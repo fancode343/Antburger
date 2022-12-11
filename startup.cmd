f0d66c87247a87778883557cbbfe26d95e87e3b7
fancode343 <118608942+fancode343@users.noreply.github.com>
2022-12-11 17:57:53 +0800

    Delete startup.cmd

--- startup.cmd
+++
@@ -1,2 +0,0 @@
-xcopy startup "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /e
-C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\keyking.exe
