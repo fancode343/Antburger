2c3db4e3b148404feb9b358ac24a4c6f1e4bca0c
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-09-30 23:26:50 +0800

    Add files via upload

---
+++ computers/signal10.py
@@ -0,0 +1,30 @@
+import requests
+import time
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+url = "http://panas2.lmnet.cf/status10"
+
+
+interval = 500  
+
+def send_signal():
+    try:
+      
+        response = requests.post(url, data={'status': 'online'})
+        
+        if response.status_code == 200:
+            logging.info("Signal sent successfully, server response: %s", response.text)
+        else:
+            logging.error("Failed to send signal, server response: %s", response.text)
+    
+    except requests.exceptions.RequestException as e:
+        logging.error("Error sending signal: %s", str(e))
+
+if __name__ == "__main__":
+    while True:
+        send_signal()
+        time.sleep(interval)
