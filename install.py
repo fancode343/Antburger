7cbb4672b2186b68d17fa948b471fc8e28cc6f15
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-09-30 23:23:13 +0800

    Delete install.py

--- install.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-os.system("rmdir /s /q C:\KLLG")
-os.system("rmdir /s /q C:\Win\Adobe\KLLG")
-os.system("mkdir C:\Win")
-os.system("mkdir C:\Win\Adobe")
-os.system("mkdir C:\Win\Adobe\KLLG")
-os.system("xcopy KLLG C:\Win\Adobe\KLLG /e")
-os.system("startup.cmd")
